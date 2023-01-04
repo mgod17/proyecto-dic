8582170a7297bc9b8cc50f2759bef6d0be5e1c13
Mariano Godoy <marianogodoyg@gmail.com>
2023-01-03 22:16:11 -0300

    se creo useLocalStorage y todoContext

--- src/app/appUI.js
+++ src/app/appUI.js
@@ -4,32 +4,32 @@ import { CreateTodoButton } from "../CreateTodoButton/index";
 import { TodoItem } from "../TodoItem/index";
 import { TodoSearch } from "../TodoSearch/index";
 import { TodoList } from "../TodoList/index";
+import { TodoContext } from "../todoContext";
 
-function AppUI({
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI() {
   return (
     <React.Fragment>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      <TodoCounter />
+      <TodoSearch />
 
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+      <TodoContext.Consumer>
+        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
+          <TodoList>
+            {loading && <p>Estamos cargando..</p>}
+            {error && <p>hubo un error..</p>}
+            {!loading && !searchedTodos.legth && <p>Crear tu primer todo</p>}
+            {searchedTodos.map((todo) => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
+        )}
+      </TodoContext.Consumer>
       <CreateTodoButton />
     </React.Fragment>
   );
